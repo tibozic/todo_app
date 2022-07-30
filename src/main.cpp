90fb80efee48ca8a00b0069dbc5bfad4718c3ebc
tibozic <timotejbozic.tb@gmail.com>
2022-07-31 00:17:44 +0200

    Creation of window using GTK.

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,31 @@
 #include <iostream>
 #include <gtk/gtk.h>
+#include "main.h"
 
-static void print_hello(GtkWidget *wigted,
-                        gpointer data)
-{
-    g_print("Hello world\n");
-}
-
-static void activate (GtkApplication *app,
-                      gpointer user_data)
-{
-    GtkWidget *window;
-    GtkWidget *button;
-
-    window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-
-    button = gtk_button_new_with_label ("Hello World");
-    g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-    gtk_window_set_child (GTK_WINDOW (window), button);
-
-    gtk_window_present (GTK_WINDOW (window));
-}
 
 int main(int argc, char **argv) {
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
+    app = gtk_application_new("my.app.todo", G_APPLICATION_FLAGS_NONE);
 
+    g_signal_connect(app, "activate", G_CALLBACK (app_activate), NULL);
+
+    status = g_application_run(G_APPLICATION (app), argc, argv);
+    g_object_unref(app);
     return status;
+}
+
+
+static void app_activate(GApplication *app,
+                            gpointer user_data)
+{
+    GtkWidget *window;
+
+    window = gtk_application_window_new(GTK_APPLICATION (app));
+    gtk_window_set_title(GTK_WINDOW (window), "My TODO App");
+    gtk_window_set_default_size(GTK_WINDOW (window), 400, 300);
+
+    gtk_widget_show(window);
+    g_print("Application is now active.\n");
 }
---
+++ src/main.h
@@ -0,0 +1,3 @@
+
+static void app_activate(GApplication *app,
+                      gpointer user_data);
