f66c6a4bb7a9a9eff137d0bfa486f2a366d34197
tibozic <timotejbozic.tb@gmail.com>
2022-07-30 12:59:20 +0200

    Open hello world window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <gtk/gtk.h>
 
-int main(int argc, char *argv[]) {
+static void print_hello(GtkWidget *wigted,
+                        gpointer data)
+{
+    g_print("Hello world\n");
+}
 
-    std::cout << "Hello, world!\n";
+static void activate (GtkApplication *app,
+                      gpointer user_data)
+{
+    GtkWidget *window;
+    GtkWidget *button;
 
-    return 0;
+    window = gtk_application_window_new (app);
+    gtk_window_set_title (GTK_WINDOW (window), "Window");
+    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+    button = gtk_button_new_with_label ("Hello World");
+    g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+    gtk_window_set_child (GTK_WINDOW (window), button);
+
+    gtk_window_present (GTK_WINDOW (window));
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+    return status;
 }
