1acf4cce8bbc7cc4d5339db667b3ce1aadb1451b
tibozic <timotejbozic.tb@gmail.com>
2022-08-01 19:42:17 +0200

    Created header file for declarations.

--- src/main.h
+++ src/main.h
@@ -1,3 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+GtkWidget *box_uncompleted_tasks;
+GtkWidget *box_completed_tasks;
+class Task;
 
 static void app_activate(GApplication *app,
                       gpointer user_data);
+
+static void app_quit(GtkButton *btn,
+                            gpointer user_data);
+
+static void task_create(GtkButton *btn,
+                            gpointer user_data);
+
+static void task_complete(GtkButton *btn,
+                            gpointer user_data);
+
+#endif
