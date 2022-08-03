03b4c4f7db56a0cb82575d646fe9dc4f35d2996f
tibozic <timotejbozic.tb@gmail.com>
2022-08-03 12:43:44 +0200

    Replaced UI code with a UI file.

--- src/main.cpp
+++ src/main.cpp
@@ -20,60 +20,17 @@ int main(int argc, char **argv) {
 static void app_activate(GApplication *app,
                             gpointer user_data)
 {
+    GtkBuilder *builder;
     GtkWidget *window;
-    GtkWidget *box_all_tasks;
-    GtkWidget *box_uncompleted_header;
-    GtkWidget *box_completed_header;
-    GtkWidget *lbl_uncompleted;
-    GtkWidget *lbl_completed;
-    GtkWidget *btn_quit;
-    GtkWidget *btn_add_task;
 
-    /* Create the window */
-    window = gtk_application_window_new(GTK_APPLICATION (app));
-    gtk_window_set_title(GTK_WINDOW (window), "My TODO App");
-    gtk_window_set_default_size(GTK_WINDOW (window), 400, 300);
-
-    box_all_tasks = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
-    gtk_box_set_homogeneous(GTK_BOX (box_all_tasks), TRUE);
-    gtk_window_set_child(GTK_WINDOW (window), box_all_tasks);
-
-    box_uncompleted_tasks = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    gtk_box_set_homogeneous(GTK_BOX (box_uncompleted_tasks), FALSE);
-    gtk_box_append(GTK_BOX (box_all_tasks), box_uncompleted_tasks);
-
-    box_completed_tasks = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    gtk_box_set_homogeneous(GTK_BOX (box_completed_tasks), FALSE);
-    gtk_box_append(GTK_BOX (box_all_tasks), box_completed_tasks);
-
-    box_uncompleted_header = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-    gtk_box_set_homogeneous(GTK_BOX (box_uncompleted_header), TRUE);
-    gtk_box_append(GTK_BOX (box_uncompleted_tasks), box_uncompleted_header);
-
-    box_completed_header = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-    gtk_box_set_homogeneous(GTK_BOX (box_completed_header), TRUE);
-    gtk_box_append(GTK_BOX (box_completed_tasks), box_completed_header);
-
-    lbl_uncompleted = gtk_label_new("Uncompleted tasks");
-    gtk_box_append(GTK_BOX (box_uncompleted_header), lbl_uncompleted);
-
-    /* Add task button */
-    btn_add_task = gtk_button_new_with_label("New task");
-    g_signal_connect(btn_add_task, "clicked", G_CALLBACK (task_create), NULL);
-    gtk_box_append(GTK_BOX (box_uncompleted_header), btn_add_task);
-
-    lbl_completed = gtk_label_new("Completed tasks");
-    gtk_box_append(GTK_BOX (box_completed_header), lbl_completed);
-
-    /* Quit button */
-    btn_quit = gtk_button_new_with_label("Quit");
-    g_signal_connect(btn_quit, "clicked", G_CALLBACK (app_quit), window);
-    gtk_box_append(GTK_BOX (box_completed_header), btn_quit);
+    builder = gtk_builder_new_from_file("../../src/ui/todo_app.ui");
+    window = GTK_WIDGET (gtk_builder_get_object(builder, "window"));
+    box_uncompleted_tasks = GTK_WIDGET (gtk_builder_get_object(builder, "box_uncompleted_tasks"));
+    box_completed_tasks = GTK_WIDGET (gtk_builder_get_object(builder, "box_completed_tasks"));
+    gtk_window_set_application(GTK_WINDOW (window), GTK_APPLICATION (app));
 
     gtk_widget_show(window);
 
-
-    std::cout << "Loading tasks.\n";
     tasks_load();
 }
 
