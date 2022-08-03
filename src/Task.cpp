2d3bbcb3f2ce383ff48c1ba0ff4dbb060eeff16e
tibozic <timotejbozic.tb@gmail.com>
2022-08-03 10:37:16 +0200

    Task: constructor now takes arguments for name, description and completness.

--- src/Task.cpp
+++ src/Task.cpp
@@ -3,7 +3,9 @@
 
 class Task {
     public: 
-        Task();
+        Task(string task_name,
+                string task_description,
+                bool task_is_complete);
 
         int get_index(void);
         string get_name(void);
@@ -17,35 +19,61 @@ class Task {
     private:
         int index;
         GtkWidget *box_task;
+        GtkWidget *btn_mark_completed;
         string name;
         string description;
         bool is_completed;
 
 };
 
-Task::Task()
+Task::Task(string task_name="",
+            string task_description="",
+            bool task_is_completed=false)
 {
-    GtkWidget *btn_mark_completed;
-    GtkWidget *task_name;
+    GtkWidget *lbl_task_name;
 
     index = all_tasks.size();
-    name = "Task" + std::to_string(index);
-    description = "Task description";
-    is_completed = false;
+
+    name = task_name;
+    description = task_description;
+    is_completed = task_is_completed;
+
+    if ( task_name == "" )
+    {
+        name = "Task" + std::to_string(index);
+    }
+
+    if ( task_description == "" )
+    {
+        description = "Task description";
+    }
 
     box_task = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
 
-    btn_mark_completed = gtk_button_new_with_label("[ ]");
+    if ( is_completed )
+    {
+        btn_mark_completed = gtk_button_new_with_label("[ ]");
+    }
+    else
+    {
+        btn_mark_completed = gtk_button_new_with_label("[X]");
+    }
+
     g_signal_connect(btn_mark_completed, "clicked", G_CALLBACK (task_complete), GINT_TO_POINTER (index));
     gtk_box_append(GTK_BOX (box_task), btn_mark_completed);
 
-    task_name = gtk_label_new(name.c_str());
+    lbl_task_name = gtk_label_new(name.c_str());
 
-    gtk_box_append(GTK_BOX (box_task), task_name);
+    gtk_box_append(GTK_BOX (box_task), lbl_task_name);
 
-    gtk_box_append(GTK_BOX (box_uncompleted_tasks), box_task);
-
-    all_tasks.push_back(*this);
+    if ( is_completed )
+    {
+        gtk_box_append(GTK_BOX (box_completed_tasks), box_task);
+    }
+    else 
+    {
+        gtk_box_append(GTK_BOX (box_uncompleted_tasks), box_task);
+    }
 }
 
 int Task::get_index(void)
